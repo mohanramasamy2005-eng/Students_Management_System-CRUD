5c4a2ff22518611ab83d32bc0a7fb5bcfb9b1719
mohanramasamy2005-eng <mohanramasamy2005@gmail.com>
2026-08-11 17:48:29 +0530

    git update

--- client/src/services/studentApi.js
+++ client/src/services/studentApi.js
@@ -1,53 +1,35 @@
-import axios from "axios";
+import axios from 'axios';
 
-// API URL
-// Local: http://localhost:5000/api
-// Render: VITE_API_URL environment variable
 const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
-/**
- * Get all students
- */
 export const getStudents = async (params = {}) => {
-  const { data } = await api.get("/students", { params });
+  const { data } = await api.get('/students', { params });
   return data;
 };
 
-/**
- * Get a single student
- */
 export const getStudent = async (id) => {
   const { data } = await api.get(`/students/${id}`);
   return data;
 };
 
-/**
- * Create a new student
- */
 export const createStudent = async (student) => {
-  const { data } = await api.post("/students", student);
+  const { data } = await api.post('/students', student);
   return data;
 };
 
-/**
- * Update a student
- */
 export const updateStudent = async (id, student) => {
   const { data } = await api.put(`/students/${id}`, student);
   return data;
 };
 
-/**
- * Delete a student
- */
 export const deleteStudent = async (id) => {
   const { data } = await api.delete(`/students/${id}`);
   return data;
